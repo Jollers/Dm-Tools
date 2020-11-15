c79032f4c54ae2dbb49f69835849a9c0cab93675
joakim * <Joakiimm@hotmail.com>
2020-11-15 15:32:29 +0100

    Initial

---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Home() {
+    return (
+        <div className="Home">
+            <a href="http://www.gay.com">Jocke är bäst!</a>
+        </div>
+    );
+};
+
+export default Home;
