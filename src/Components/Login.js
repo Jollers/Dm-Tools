d4eac18ef5ad36515066a97b4cf755772698fe61
Nayomi Arvell <nayomi.arvell@gmail.com>
2021-01-18 19:44:12 +0100

    Adds login component

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -4,23 +4,21 @@ import Form from "react-bootstrap/Form";
 
 function Login() {
   return (
-    <div>
-      <Form>
-        <Col xs="2">
-          <Form.Group>
-            <Form.Label>Login</Form.Label>
-            <Form.Control 
-            type="email" 
-            placeholder="Enter email" />
-          </Form.Group>
-          <Form.Group controlId="formGroupPassword">
-            <Form.Control 
-            type="password" 
-            placeholder="Password" />
-          </Form.Group>
-          <Button>Submit</Button>
-        </Col>
-      </Form>
+    <div class="container">
+      <div class="content-align-center">
+        <Form class="">
+          <Col xs="4">
+            <Form.Group>
+              <Form.Label class="text-sm-left">Login</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+            <Form.Group controlId="formGroupPassword">
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Button>Submit</Button>
+          </Col>
+        </Form>
+      </div>
     </div>
   );
 }
