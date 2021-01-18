e67d608477232970d86d9d2642e1b44a01f03ca0
joakim * <Joakiimm@hotmail.com>
2021-01-18 19:53:16 +0100

    Merge första

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import './App.css';
 import Home from './Components/Home';
-import Navigationbar from './Components/Navigationbar';
+import Characters from './Components/Characters/Characters';
+import Navigationbar from './Components/Navigationbar/Navigationbar';
+import Create from './Components/Characters/Create';
+import Music from './Components/Music/Music';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './Components/Login';
 
 function App() {
   return (
-    <div className="App">
-    <Navigationbar />
-     <Home />
-     <Login />
-    </div>
+    <Router>
+      <div className="App">
+        <Navigationbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/Characters" component={Characters} />
+          <Route path="/Music" component={Music} />
+          <Route path="/Create" component={Create} />
+        </Switch>
+
+      </div>
+    </Router>
   );
 }
 export default App;
